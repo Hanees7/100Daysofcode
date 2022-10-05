d2584f7a71cd9e95f03ae3d5442ee717a40181f8
hanees7 <mohammedhanees2022@vitbhopal.ac.in>
2022-10-06 00:59:12 +0530

    Day 6

---
+++ arayy.java
@@ -0,0 +1,11 @@
+public class arayy {
+    public static void main(String[] args) {
+        int s[]={1,2,3,4,5,6,7,8,9,10};
+        for(int t=0;t<s.length;t++)
+        {
+            System.out.println(s[t]);
+        }
+        
+    }
+}
+
