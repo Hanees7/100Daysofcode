eaca51b68674d67eb30789948ba7734836b6b337
hanees7 <mohammedhanees2022@vitbhopal.ac.in>
2022-11-01 20:35:38 +0530

    Day 13

---
+++ Patttern2.java
@@ -0,0 +1,26 @@
+public class Patttern2 {
+    public static void main(String[] args) {
+   int c=1;
+        for(int a=5;a>=1;a--)
+   {
+    for(int b=1;b<=a;b++)
+   
+{
+    
+    
+System.out.print(c);
+
+
+}
+c=c+1;
+System.out.println(" ");}
+    }
+}
+
+
+
+
+
+//System.out.print("*");
+    // System.out.print(b);
+    //System.out.print(a);
