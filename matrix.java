4ed9eb97ecbe30fd481df7cd2184313ce5158dd4
Mohammed Hanees <mohmmadhanees786@gmail.com>
2022-11-05 02:26:45 +0530

    Day 16

---
+++ matrix.java
@@ -0,0 +1,16 @@
+public class matrix {
+    public static void main(String[] args) {
+        int [][]a={
+            {1,2,3},
+            {2,3,4},
+            {3,4,5}
+        };
+for(int i=0;i<a.length;i++){
+    for(int j=0;j<a[i].length;j++){
+    System.out.print(a[i][j]+" ");
+    }
+    System.out.println("");
+}
+
+    }
+}
