3da27543b4070a92b73902021840c490cf047725
Mohammed Hanees <mohmmadhanees786@gmail.com>
2022-11-09 15:14:43 +0530

    Day 21

---
+++ sleep.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.Thread;
+
+class sleep {
+	public static void main(String[] args)
+	{
+		try {
+			for (int i = 0; i <=5; i++) {
+		
+				Thread.sleep(1000);
+				System.out.println(i);
+			}
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
