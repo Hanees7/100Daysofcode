7c4a59d90fc608290c22ad76d4c3955a110e4aa7
Mohammed Hanees <mohmmadhanees786@gmail.com>
2022-11-10 19:29:43 +0530

    Day 22

---
+++ LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int arr[]={1,45,3,4,7,8,67};
+        int key=7;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+                System.out.println("Position is: "+i);
+                
+            }
+            
+            }
+            
+                
+        
+    }
+}
