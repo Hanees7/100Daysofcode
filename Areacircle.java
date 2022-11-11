d27413a666ceaae5f1671760b226cc134e1011e3
Mohammed Hanees <mohmmadhanees786@gmail.com>
2022-11-11 20:15:04 +0530

    Day 23

---
+++ Areacircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Areacircle {
+ public static void main(String[] args) {
+    Scanner s1=new Scanner(System.in);
+    System.out.println("Enter the Value");
+    float r=s1.nextFloat();
+    float sum=3.14f * r * r;
+    System.out.println(sum);
+ }   
+}
+ 
