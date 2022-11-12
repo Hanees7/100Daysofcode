4dcfafc37559a5b58fe5a341a7eb004c8fb7a13a
Mohammed Hanees <mohmmadhanees786@gmail.com>
2022-11-12 21:36:08 +0530

    Day 24

---
+++ Typecasting.java
@@ -0,0 +1,9 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        double h=7;
+        byte a=(byte)(h*2);
+        int b=(int)(h+5);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
