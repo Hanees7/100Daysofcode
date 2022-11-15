ca83ed2665edb23b20755dfbf26814a14a654b01
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-11-15 21:19:33 +0530

    Day 27

---
+++ gfg.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Solution ob = new Solution();
+        ob.printHelloWorld();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    static void printHelloWorld(){
+        // Scanner sc=new Scanner(System.in);
+        // String a=sc.nextLine();
+        // System.out.print(a);
+        // code here
+        System.out.println("Hello World");
+    }
+}
