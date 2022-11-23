ea5ca251177f3e7941ce140a493d6336e7147e38
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-11-23 23:59:47 +0530

    Day 35

---
+++ Sumofnum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Alpha14 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+System.out.println("Enter the Number");
+        int a=1;
+        int sum=0;
+        int b=sc.nextInt();
+        while(a<=b){
+           
+            sum+=a;
+            a++;
+            
+            
+
+        }
+        System.out.println(sum);
+    }
+}
