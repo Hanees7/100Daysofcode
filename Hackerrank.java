c07505fd2c5c4b18affd09f0776468d66a29438c
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-11-25 21:23:28 +0530

    Day 37

---
+++ Hackerrank.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       if( n<=-100  && n<=100 ){
+           System.out.println("Good job");
+       }
+       else{
+         System.out.print("Wrong answer");  
+       }
+       
+       }
+}
