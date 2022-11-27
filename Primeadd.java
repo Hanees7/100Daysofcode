0581a4b98a9322917b22c20b484156863f040db1
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-11-27 23:22:26 +0530

    Day 39

---
+++ Primeadd.java
@@ -0,0 +1,29 @@
+public class Alpha45 {
+
+public static boolean isprime(int a){
+
+    if(a==2){
+    return true;
+}
+for(int i=2;i<=a-1;i++){
+if(a%i==0){
+return false;
+}
+}
+return true;
+}
+    public static void primesum(int n){
+for(int i=2;i<=n;i++){
+if(isprime(i)){
+    System.out.print(i+" ");
+}
+}
+System.out.println(" ");
+   }
+   public static void main(String[] args) {
+    primesum(20);
+   }
+
+
+
+}
