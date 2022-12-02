6d8f6442f2a34238da1f880541dc4feb6cfb92c9
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-12-02 23:46:49 +0530

    Day 44

---
+++ Floyd.java
@@ -0,0 +1,18 @@
+public class Floyd {
+    
+public static void floyed(int n){
+    int counter=1;
+    for(int i=1; i<=n;i++){
+for(int j=1; j<=i; j++){
+    System.out.print(counter + " ");
+    counter++;
+}
+System.out.println("");
+ }
+}
+public static void main(String[] args) {
+    floyed(5);
+}
+
+
+}
