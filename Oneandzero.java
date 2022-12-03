dff3f43fb38a6964f643ebba1b46f9b50ce08974
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2022-12-03 22:53:52 +0530

    Day 45

---
+++ Oneandzero.java
@@ -0,0 +1,21 @@
+public class OneAndzero {
+    public static void oneAnd0(int n){
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+if((i+j)%2==0){
+    System.out.print("1");
+}
+else{System.out.print("0");
+}
+
+
+    }
+System.out.println("");
+}
+
+
+    } 
+public static void main(String[] args) {
+    oneAnd0(5);
+}
+}
