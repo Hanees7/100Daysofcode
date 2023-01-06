7165e08141f99ad98a9d6a2aa36fae7b48488012
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-01-06 18:36:26 +0530

    Day 57

---
+++ SumOFDigit.java
@@ -0,0 +1,36 @@
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  a and b 
+
+Constraints
+
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+
+OUTPUT :-
+  
+  
+  import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=a+b;
+       System.out.println(c);
+    }
+}
