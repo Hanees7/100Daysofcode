501692a369215c0a5fbd553b17fadfac229f2e3b
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-01-21 01:54:52 +0530

    Day 72

---
+++ BirthdayCakeCandles.java
@@ -0,0 +1,64 @@
+
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer, , the size of .
+The second line contains  space-separated integers, where each integer  describes the height of .
+
+Constraints
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are . The tallest candles are  units, and there are  of them.
+  
+  OUTPUT:- 
+    
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int count=0;
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0; i<a; i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        int max=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            if(max==arr[i]){
+                
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
