b79b11b43afff96c13edcf9c9bda0d03d6cc166d
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-01-27 00:07:02 +0530

    Day 77

--- Java_SubString_Comparisons.java
+++ Java_SubString_Comparisons.java
@@ -1,3 +1,51 @@
+We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Function Description
+
+Complete the getSmallestAndLargest function in the editor below.
+
+getSmallestAndLargest has the following parameters:
+
+string s: a string
+int k: the length of the substrings to find
+Returns
+
+string: the string ' + "\n" + ' where and are the two substrings
+Input Format
+
+The first line contains a string denoting .
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+Explanation 0
+
+String  has the following lexicographically-ordered substrings of length :
+
+We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring as two newline-separated values (i.e., ava\nwel).
+
+The stub code in the editor then prints ava as our first line of output and wel as our second line of output.
+    
+    
+    OUTPUT:- 
+
+
+
 import java.util.Scanner;
 
 public class Solution {
@@ -29,6 +77,38 @@ public class Solution {
          
         
 
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
+
+
+
+OR
+
+
+
+(Only 2 test Cases Pass)
+import java.util.Scanner;
+
+public class Solution {
+
+public static String getSmallestAndLargest(String s, int k){
+    
+    String c=s.substring(0,k);
+    String d=s.substring(s.length()-k,s.length());
+    
+    //String e=d+"\n"+c;
+    return d+"\n"+c;
+    
+    
+}
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
