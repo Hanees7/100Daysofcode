fe34d4135ee1208f0cf37139d2d56346792f045d
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-02-03 23:00:28 +0530

    Day 85

---
+++ Java_Interface.java
@@ -0,0 +1,45 @@
+A Java interface can only contain method signatures and fields. The interface can be used to achieve polymorphism. In this problem, you will practice your knowledge on interfaces.
+
+You are given an interface AdvancedArithmetic which contains a method signature int divisor_sum(int n). You need to write a class called MyCalculator which implements the interface.
+
+divisorSum function just takes an integer as input and return the sum of all its divisors. For example divisors of 6 are 1, 2, 3 and 6, so divisor_sum should return 12. The value of n will be at most 1000.
+
+Read the partially completed code in the editor and complete it. You just need to write the MyCalculator class only. Your class shouldn't be public.
+
+Sample Input
+
+6
+Sample Output
+
+I implemented: AdvancedArithmetic
+12
+Explanation
+
+Divisors of 6 are 1,2,3 and 6. 1+2+3+6=12.
+  
+  OUTPUT :-
+    
+    
+    import java.io.*;
+import java.util.*;
+
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int temp=0;
+        for(int i=1; i<=a; i++){
+            if(a%i==0){
+                temp+=i;
+            }
+        }
+    System.out.println("I implemented: AdvancedArithmetic");
+    System.out.println(temp);
+    }
+}
