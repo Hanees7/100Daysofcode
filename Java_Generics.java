acbba3e6baffa3c41ccf3050d1d4a4b5d2450dd8
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-02-18 23:39:53 +0530

    Day 100

---
+++ Java_Generics.java
@@ -0,0 +1,37 @@
+Generic methods are a very efficient way to handle multiple datatypes using a single method. This problem will test your knowledge on Java Generic methods.
+
+Let's say you have an integer array and a string array. You have to write a single method printArray that can print all the elements of both arrays. The method should be able to accept both integer arrays or string arrays.
+
+You are given code in the editor. Complete the code so that it prints the following lines:
+
+1
+2
+3
+Hello
+World
+Do not use method overloading because your answer will not be accepted.
+  
+  OUTPUT :-
+    
+    import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int a=1;
+        int b=2;
+        int c=3;
+        String d="Hello";
+        String e="World";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+        
+        
+    }
+}
