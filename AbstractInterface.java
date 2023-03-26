dc2c43033e969b49d94e2a4df22e3d9b4e38b661
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-03-26 21:16:37 +0530

    Day 112

---
+++ AbstractInterface.java
@@ -0,0 +1,29 @@
+public class AbsTractInterfaces {
+    public static void main(String[] args) {
+        Queen q1 = new Queen();
+        q1.moves();
+    }
+}
+
+interface ChessPlay {
+    // fx Blueprint
+    void moves();
+}
+
+class Queen implements ChessPlay {
+    public void moves() {
+        System.out.println("up, down, left, right, diagonal");
+    }
+}
+
+class Rook implements ChessPlay {
+    public void moves() {
+        System.out.println("up, down, left, right, diagonal");
+    }
+}
+
+class King implements ChessPlay {
+    public void moves() {
+        System.out.println("up, down, left, right, Diagonal by 1");
+    }
+}
