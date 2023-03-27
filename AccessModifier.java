7cf304deb88726cb63a7623d8cf4158ee9dde492
Mohammed Hanees <114352958+Hanees7@users.noreply.github.com>
2023-03-27 22:07:56 +0530

    Day 113

---
+++ AccessModifier.java
@@ -0,0 +1,19 @@
+public class AccessModi {
+    public static void main(String[] args) {
+        Bank b1 = new Bank();
+        b1.User = "Hanees";
+        System.out.println(b1.User);
+        b1.pas(786);
+    }
+}
+
+class Bank {
+    String User;
+    private int pass;
+
+    void pas(int a) { // public void pas(int a)
+        pass = a;
+        System.out.print(pass);
+    }
+
+}
